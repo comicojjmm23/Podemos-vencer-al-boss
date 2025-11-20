f71fa3d2689e64265341b4284d0e76922e9943d9
comicojjmm23 <jesusmartinezreivajkm3@gmail.com>
2025-11-19 21:39:15 -0400

    forzamos

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,9 +3,9 @@
 // Archivo de configuración para la URL de la API
 // =======================================================
 
-// Lee la variable de entorno inyectada por Vercel (Production)
-// o la variable de entorno local (Development)
-const VERCEL_API_URL = process.env.REACT_APP_API_URL;
+// AHORA BUSCAMOS LA VARIABLE CON EL PREFIJO ESTÁNDAR DE VERCEL/NEXT.JS
+// (usamos este prefijo para forzar la inyección en la fase de build)
+const VERCEL_API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Definimos la URL de la API.
 // Priorizamos la variable de entorno de Vercel si existe.
